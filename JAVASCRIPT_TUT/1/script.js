28ec54ecd425331c68d758cec085a35b2578e787
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-26 00:50:02 +0530

    add: javascript frontend

---
+++ JAVASCRIPT_TUT/1/script.js
@@ -0,0 +1,20 @@
+alert("Hello World");
+
+console.log("Code was running....")
+console.log("Code is running....")
+console.log("Code will running....")
+
+var a = prompt("Enter a number: ")
+var isTrue = confirm("are you gonna get a job")
+
+if(isTrue){
+    console.log("You will get the job !")
+}
+else{
+    console.log("You will still get one.")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey i am great"
+document.body.style.background = "aqua"
