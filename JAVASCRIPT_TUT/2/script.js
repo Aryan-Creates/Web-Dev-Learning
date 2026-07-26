edc5c8eb4a0582e196920ed0c75db727829ea1b8
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-26 18:29:11 +0530

    add: JavaScript data types js

---
+++ JAVASCRIPT_TUT/2/script.js
@@ -0,0 +1,33 @@
+console.log("Okayyyy")
+
+var a = 69;
+var b = 96;
+var _c = "wuhuuuu";
+{
+    let a = 123;
+    console.log(a)
+}
+console.log(a)
+console.log(a + b + 1)
+console.log(typeof a, typeof b, typeof c)
+
+let x = 300;
+let y = 21.2123;
+let z = "Aur bhai";
+
+const s = false;
+const t = undefined;
+const r = null;
+
+console.log(x, y, z, s, t, r)
+console.log(typeof(x), typeof(y), typeof(z), typeof(s), typeof(t), typeof(r))
+
+let whoosh = {
+    "name" : "Aryan",
+    "RollNo." : 113
+}
+
+console.log(whoosh)
+
+whoosh.age = 19
+console.log(whoosh)
