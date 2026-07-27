23e5e2c17c3c48d39eef54f1692dc1d1bfe33f90
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-27 23:47:03 +0530

    add: conditional ops js

---
+++ JAVASCRIPT_TUT/3/index.js
@@ -0,0 +1,27 @@
+console.log("This is conditional tutorial ! ")
+
+let a = 72;
+// let grace = 1;
+
+// console.log(a+grace)
+// console.log(a-grace)
+// console.log(a*grace)
+// console.log(a/grace)
+// console.log(a%grace)
+// console.log(a**grace)
+
+if(a == 18){
+    console.log("Mature");
+}
+else if(a == 1){
+    console.log("seriously");
+}
+else{
+    console.log("Not mature");
+}
+
+b = 13;
+c = 43;
+
+let f = b < c ? (1) : (0);
+console.log(f)
