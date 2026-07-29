d7562f0051ab48479c3df151341b72edf3030160
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-30 01:09:35 +0530

    add: strings in js

---
+++ JAVASCRIPT_TUT/6/index.js
@@ -0,0 +1,25 @@
+console.log("THis is strings tutorial")
+let a = "Aryan";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length)
+
+let real_name = "Aryan"
+let friend = "Honey"
+console.log("His name is " + real_name + " and his friend is " + friend)
+console.log('His name is  ${real_name} and his friend is ${friend}')
+
+let b = "BigBoy"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 3))
+console.log(b.slice(1))
+
+console.log(b.replace("Big", 1))
+console.log(b.concat(a, "Bro", "Cool"))
+console.log(b);
