6d6685c8832174277117dcac7e6d9ed9e06f66cc
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-30 23:58:24 +0530

    add: arrays in js

---
+++ JAVASCRIPT_TUT/8/index.js
@@ -0,0 +1,34 @@
+let arr = [1, 2, 4, 5, 6]
+
+arr[3] = 55;
+// console.log(arr, typeof(arr));
+// console.log(arr.length);
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// console.log(arr.toString())
+// console.log(arr.join(" : "))
+// console.log(arr.pop())
+// console.log(arr.shift())
+// console.log(arr.unshift(9,0))
+// console.log(arr.push(111, 123))
+
+let arr1 = [23, 5, 57, 234]
+
+console.log(arr1)
+console.log(arr)
+console.log(arr.concat(arr1));
+
+console.log(arr1.sort())
+console.log(arr.sort())
+
+console.log(arr1)
+
+console.log(arr1.splice(5, 234, 1111))
+console.log(arr1)
+console.log(arr1.slice(2, 4))
+console.log(arr1)
