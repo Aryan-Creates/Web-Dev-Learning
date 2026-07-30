ffcbcf45840c2bbf480b61b2582bef3172552d50
Aryan Gupta <guptaaryan4501@gmail.com>
2026-07-30 23:58:42 +0530

    add: loops with arrays in js

---
+++ JAVASCRIPT_TUT/8/loops.js
@@ -0,0 +1,26 @@
+let a = [23, 24, 21, 244]
+
+for(let index = 0; index < a.length; index++){
+    const element = a[index];
+    console.log(element)
+}
+
+a.forEach((value, index, arr)=>{
+    console.log(value, index, arr)
+})
+
+let obj = {
+    a : 1,
+    b : 2,
+    c : 3
+}
+for(const key in obj){
+    if(Object.hasOwnProperty.call(obj, key)){
+        const element = obj[key];
+        console.log(key, element)
+    }
+}
+
+for(const value of a){
+    console.log(value);
+}
